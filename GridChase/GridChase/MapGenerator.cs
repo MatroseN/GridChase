19f82b54c6a5d908dc3b2ac8ab538962217c2438
felix <felix.lundh@gmail.com>
2020-09-09 18:19:35 +0200

    map generator class

--- GridChase/GridChase/MapGenerator.cs
+++ GridChase/GridChase/MapGenerator.cs
@@ -1,9 +1,27 @@
 ﻿
 
+using Microsoft.Xna.Framework;
+
 namespace GridChase {
     /* 
      Responsible for setting the different positions on the map depending on data fetched from a textfile
     */
     class MapGenerator {
+        public MapGenerator() {
+
+        }
+
+        public int[] generateGrid(Vector2 mapSize) {
+            // Initialize the grid array to fit all map positions
+            int[] grid = new int[(int)mapSize.X * (int)mapSize.Y];
+
+            for (int x = 0; x < (int)mapSize.X; x++) {
+                for (int y = 0; y < (int)mapSize.Y; y++) {
+                    grid[y * (int)mapSize.X + x] = 1;
+                }
+            }
+
+            return grid;
+        }
     }
 }

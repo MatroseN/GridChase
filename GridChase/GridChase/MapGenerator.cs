1000448657deb310f167459987564b4676aaf6a0
felix <felix.lundh@gmail.com>
2020-09-09 17:11:59 +0200

    Explanatory comment

--- GridChase/GridChase/MapGenerator.cs
+++ GridChase/GridChase/MapGenerator.cs
@@ -1,6 +1,9 @@
 ﻿
 
 namespace GridChase {
+    /* 
+     Responsible for setting the different positions on the map depending on data fetched from a textfile
+    */
     class MapGenerator {
     }
 }
